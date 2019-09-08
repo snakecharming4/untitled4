eb3f7c84b10d8f4d37b182bfac3c98259a26981c
snakecharming4 <snakecharming4@gmail.com>
2019-09-08 11:42:16 +0200

    mojazmiana

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+            System.out.println("Hello World");
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.booking.com");
+            System.out.println(driver.getTitle());
+            WebElement element = driver.findElement(By.className("sb-destination__input"));
+            System.out.println(element.getAttribute("placeholder"...);
+        }
+    }
